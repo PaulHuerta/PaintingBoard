11eb74d6cae88482e334a049808e40a111f6ffb3
PaulHuerta <paulhuerta888@gmail.com>
2021-03-24 13:45:47 -0600

    Update

--- js/index.js
+++ js/index.js
@@ -3,24 +3,24 @@ window.addEventListener("load", () => {
   const ctx = canvas.getContext("2d");
 
   canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight * 0.80;
+  canvas.height = window.innerHeight * 0.8;
 
-  var color = "black", canvasWidth = 5;
+  var color = "black",
+    canvasWidth = 5;
   let painting = false;
 
   var inputColor = document.getElementById("color");
   var inputWidth = document.getElementById("width");
 
-  inputColor.oninput = function(){
+  inputColor.oninput = function () {
     color = this.value;
     console.log(color);
-  }
-  
-  inputWidth.oninput = function(){
+  };
+
+  inputWidth.oninput = function () {
     canvasWidth = this.value;
     console.log(canvasWidth);
-  }
-  
+  };
 
   function startPosition() {
     painting = true;
@@ -43,7 +43,18 @@ window.addEventListener("load", () => {
     ctx.beginPath();
     ctx.moveTo(e.clientX, e.clientY);
   }
-  canvas.addEventListener("mousedown", startPosition);
-  canvas.addEventListener("mouseup", finishedPosition);
-  canvas.addEventListener("mousemove", draw);
+
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    canvas.addEventListener("touchstart", startPosition);
+    canvas.addEventListener("touchend", finishedPosition);
+    canvas.addEventListener("touchmove", draw);
+  } else {
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw);
+  }
 });
